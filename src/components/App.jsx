aeb46e8764e1702f538e72a52d75d9a36a876558
Win <win.eiwwongcharoen@gmail.com>
2017-04-28 18:50:07 +0700

    Add quiz

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import Quiz from './quiz/Quiz'
+import AddQuiz from './quiz/AddQuiz'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <AddQuiz />
+        <hr/>
         <Quiz />
       </div>
     )
--- src/components/quiz/AddQuiz.jsx
+++ src/components/quiz/AddQuiz.jsx
@@ -17,14 +17,29 @@ class AddQuiz extends Component {
   }
 
   render() {
+    const lists = []
+    for (var i = 0; i < this.state.amount; i++) {
+      lists.push(
+        <div key={i}>
+          <input type="text" /><br />
+          - <input type="text" /><br />
+          - <input type="text" /><br />
+          - <input type="text" /><br />
+          <br />
+        </div>
+      )
+    }
+
+
     return (
       <div>
         <form>
           <label htmlFor="">Number of quizes</label>
           <input type="number" value={this.state.amount} onChange={(e) => this.handleInput(e)} />
         </form>
+        <br/>
 
-        { lists }
+        {lists}
       </div>
     )
   }
