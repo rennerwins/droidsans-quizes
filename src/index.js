1d60de64ba183eddc597f3dcccd8c54140c06963
Win <win.eiwwongcharoen@gmail.com>
2017-04-28 16:13:50 +0700

    Fix error

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-console.log(user-agent)
-
 ReactDOM.render(
   <App />,
   document.getElementById('root')
